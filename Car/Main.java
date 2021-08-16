725b5052cdc8063b9fbacbfa8b5c3a6e7ba68722
Harout <harout-dev@gmail.com>
2021-08-16 14:29:55 +0300

    Adding instance variables

--- Car/Main.java
+++ Car/Main.java
@@ -7,6 +7,15 @@ public class Main{
     public static void main(final String[] args) {
 
         Car car1 = new Car();
+
+        car1.make = "Ford";
+        System.out.println("Car1 make: " + car1.make);
+        System.out.println("Count of objects: " + (++Car.count));
+        System.out.println();
+        Car car2 = new Car();
+        car2.make = "Chevy";
+        System.out.println("Car2 make: " + car2.make);
+        System.out.println("Count of objects: " + (++Car.count));
     
     } 
 
--- Car/com/inventory/list/Car.java
+++ Car/com/inventory/list/Car.java
@@ -1,7 +1,13 @@
 package com.inventory.list;
 
-
+import java.math.BigDecimal;
 
 public class Car{
 
+    public static int count = 0;
+    
+    public String make;
+    public String model;
+    public String color;
+    public BigDecimal price;
 }
